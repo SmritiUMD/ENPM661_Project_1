ae1d24bf3d9b550a16238db038a62d5d4566afe9
SmritiUMD <sgupta23@terpmail.umd.edu>
2020-02-21 12:24:14 -0500

    Defined a function to check all possible move and generat matrices corresponding to it.

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -15,7 +15,24 @@ def print_blank(a):
 		for k,l in enumerate(j):
 			if l==0:
 				return i,k
-
+def swapping(data,i,k):
+	keys={(0,0):[(0,1),(1,0)],
+		  (0,1):[(0,0),(0,2),(1,1)],
+		  (0,2):[(0,1),(1,2)],
+		  (1,0):[(0,0),(1,1),(2,0)],
+		  (1,1):[(0,1),(1,0),(1,2),(2,1)],
+		  (1,2):[(0,2),(1,1),(2,2)],
+		  (2,0):[(1,0),(2,1)],
+		  (2,1):[(1,1),(2,0),(2,2)],
+		  (2,2):[(1,2),(2,1)]}
+	possible=[]
+	for set_ in keys.get((i,k)):
+		temp_data = np.copy(data)
+		temp=temp_data[set_]
+		temp_data[set_]=temp_data[i,k]
+		temp_data[i,k]=temp  
+		possible.append(temp_data)    
+	return possible
 
 
 
