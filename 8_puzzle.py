118beb737f15f51b7bdb96da24724b4062b8df00
SmritiUMD <sgupta23@terpmail.umd.edu>
2020-02-21 15:15:49 -0500

    Define parent and own id's of nodes and stored them into a list

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -34,6 +34,51 @@ def swapping(data,i,k):
 		possible.append(temp_data)    
 	return possible
 
+ds=[]
+	ds.append([a,own_id,parent_id])
+	flag= False
+	i=0
+	while (not flag):
+		parent= ds[i]
+		print('i= ',i)
+		next_iter= swapping(parent[0],print_blank(parent[0])[0],print_blank(parent[0])[1])
+		node_pid= parent[1]
+		j=0
+		while(j<len(next_iter)):
+			unq= True
+			for k in range(len(ds)):
+				# comparing the possible swaps with already stored values id ds.
+				comparison= next_iter[j]== ds[k][0]
+				if(comparison.all()==True):
+					unq= False
+					del next_iter[j]
+					j-=1
+					break
+			if(unq):
+				node_oid= len(ds)
+				# appending all unique combinations in ds.
+				ds.append([next_iter[j],node_oid, node_pid])
+				# writing node information in the text file(converting arrays into string)
+				nodes=str(next_iter[j][0][0])+" "+str(next_iter[j][1][0])+" "+str(next_iter[j][2][0])+" "
+				nodes+=str(next_iter[j][0][1])+" "+str(next_iter[j][1][1])+" "+str(next_iter[j][2][1])+" "
+				nodes+=str(next_iter[j][0][2])+" "+str(next_iter[j][1][2])+" "+str(next_iter[j][2][2])+"\n"
+				Nodes.write(nodes)
+				
+				# comparing iterations with goal matrix 
+				comparison_g=next_iter[j]==goal
+				if(comparison_g.all()==True):
+					# ds.append([next_iter[j],node_oid, node_pid])
+					print('Goal reached')
+					flag = True
+					solvable=False
+					break 	
+			j+=1
+		if(i<len(ds)-1):						
+			i+=1
+		else:
+			break
+		parent= ds[i]
+
 
 
 
